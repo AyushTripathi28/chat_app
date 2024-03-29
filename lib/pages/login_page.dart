e7b84ec48877029a3415b1084a2eab0f95e98a4a
Ayush Tripathi <ayushtripathi445@gmail.com>
2024-03-29 06:02:17 +0530

    push complete app

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -57,97 +57,102 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //welcome back message
-            const Spacer(
-              flex: 5,
-            ),
-            SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 35.0),
-                    child: Text("Login",
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //welcome back message
+              const Spacer(
+                flex: 5,
+              ),
+              SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 35.0),
+                      child: Text("Login",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 35.0),
+                      child: Text(
+                        "Please login to continue",
                         style: TextStyle(
                           color: Theme.of(context).colorScheme.primary,
-                          fontSize: 35,
-                          fontWeight: FontWeight.bold,
-                        )),
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    //email
+                    MyTextField(
+                      hintText: "Email",
+                      obscureText: false,
+                      controller: _emailController,
+                      icon: Icons.email_outlined,
+                    ),
+
+                    const SizedBox(height: 10),
+
+                    //pw textfield
+                    MyTextField(
+                      hintText: "Password",
+                      obscureText: true,
+                      controller: _passwordController,
+                      icon: Icons.lock_outline,
+                    ),
+
+                    const SizedBox(height: 25),
+                    //login btn
+
+                    MyButton(
+                      text: "Login",
+                      onTap: () => login(context),
+                    ),
+
+                    const SizedBox(height: 25),
+                  ],
+                ),
+              ),
+              const Spacer(
+                flex: 4,
+              ),
+              //register row
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Not a member? ",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 35.0),
+                  GestureDetector(
+                    onTap: widget.onTap,
                     child: Text(
-                      "Please login to continue",
+                      "Register Now",
                       style: TextStyle(
-                        color: Theme.of(context).colorScheme.primary,
-                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Theme.of(context).colorScheme.tertiary,
                       ),
                     ),
                   ),
-                  const SizedBox(height: 30),
-                  //email
-                  MyTextField(
-                    hintText: "Email",
-                    obscureText: false,
-                    controller: _emailController,
-                    icon: Icons.email_outlined,
-                  ),
-
-                  const SizedBox(height: 10),
-
-                  //pw textfield
-                  MyTextField(
-                    hintText: "Password",
-                    obscureText: true,
-                    controller: _passwordController,
-                    icon: Icons.lock_outline,
-                  ),
-
-                  const SizedBox(height: 25),
-                  //login btn
-
-                  MyButton(
-                    text: "Login",
-                    onTap: () => login(context),
-                  ),
-
-                  const SizedBox(height: 25),
                 ],
               ),
-            ),
-            const Spacer(
-              flex: 4,
-            ),
-            //register row
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Not a member? ",
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.secondary,
-                  ),
-                ),
-                GestureDetector(
-                  onTap: widget.onTap,
-                  child: Text(
-                    "Register Now",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      color: Theme.of(context).colorScheme.tertiary,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const Spacer(),
-          ],
+              const Spacer(),
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -63,121 +63,126 @@ class RegisterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Center(
-        child: SingleChildScrollView(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              minWidth: MediaQuery.of(context).size.width,
-              minHeight: MediaQuery.of(context).size.height,
-            ),
-            child: IntrinsicHeight(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Spacer(
-                    flex: 5,
-                  ),
-                  SingleChildScrollView(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 35.0),
-                          child: Text("Register",
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Center(
+          child: SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minWidth: MediaQuery.of(context).size.width,
+                minHeight: MediaQuery.of(context).size.height,
+              ),
+              child: IntrinsicHeight(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Spacer(
+                      flex: 5,
+                    ),
+                    SingleChildScrollView(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 35.0),
+                            child: Text("Register",
+                                style: TextStyle(
+                                  color: Theme.of(context).colorScheme.primary,
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 35.0),
+                            child: Text(
+                              "Let's create a account for you",
                               style: TextStyle(
                                 color: Theme.of(context).colorScheme.primary,
-                                fontSize: 35,
-                                fontWeight: FontWeight.bold,
-                              )),
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 30),
+                          MyTextField(
+                            hintText: "Name",
+                            obscureText: false,
+                            controller: _nameController,
+                            icon: Icons.person_outline,
+                          ),
+
+                          const SizedBox(height: 10),
+                          MyTextField(
+                            hintText: "Email",
+                            obscureText: false,
+                            controller: _emailController,
+                            icon: Icons.email_outlined,
+                          ),
+
+                          const SizedBox(height: 10),
+
+                          //pw textfield
+                          MyTextField(
+                            hintText: "Password",
+                            obscureText: true,
+                            controller: _passwordController,
+                            icon: Icons.lock_outline,
+                          ),
+
+                          const SizedBox(height: 10),
+
+                          //confirm pw textfield
+                          MyTextField(
+                            hintText: "Confirm Password",
+                            obscureText: true,
+                            controller: _confirmPasswordController,
+                            icon: Icons.lock_outline,
+                          ),
+
+                          const SizedBox(height: 25),
+                          //login btn
+
+                          MyButton(
+                            text: "Register",
+                            onTap: () => register(context),
+                          ),
+
+                          const SizedBox(height: 25),
+                        ],
+                      ),
+                    ),
+                    const Spacer(
+                      flex: 4,
+                    ),
+                    //register row
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Already have a account? ",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 35.0),
+                        GestureDetector(
+                          onTap: onTap,
                           child: Text(
-                            "Let's create a account for you",
+                            "Login Now",
                             style: TextStyle(
-                              color: Theme.of(context).colorScheme.primary,
-                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).colorScheme.tertiary,
                             ),
                           ),
                         ),
-                        const SizedBox(height: 30),
-                        MyTextField(
-                          hintText: "Name",
-                          obscureText: false,
-                          controller: _nameController,
-                          icon: Icons.person_outline,
-                        ),
-
-                        const SizedBox(height: 10),
-                        MyTextField(
-                          hintText: "Email",
-                          obscureText: false,
-                          controller: _emailController,
-                          icon: Icons.email_outlined,
-                        ),
-
-                        const SizedBox(height: 10),
-
-                        //pw textfield
-                        MyTextField(
-                          hintText: "Password",
-                          obscureText: true,
-                          controller: _passwordController,
-                          icon: Icons.lock_outline,
-                        ),
-
-                        const SizedBox(height: 10),
-
-                        //confirm pw textfield
-                        MyTextField(
-                          hintText: "Confirm Password",
-                          obscureText: true,
-                          controller: _confirmPasswordController,
-                          icon: Icons.lock_outline,
-                        ),
-
-                        const SizedBox(height: 25),
-                        //login btn
-
-                        MyButton(
-                          text: "Register",
-                          onTap: () => register(context),
-                        ),
-
-                        const SizedBox(height: 25),
                       ],
                     ),
-                  ),
-                  const Spacer(
-                    flex: 4,
-                  ),
-                  //register row
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Already have a account? ",
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.primary,
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: onTap,
-                        child: Text(
-                          "Login Now",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).colorScheme.tertiary,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const Spacer(),
-                ],
+                    const Spacer(),
+                  ],
+                ),
               ),
             ),
           ),
